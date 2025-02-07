85204c754a3d71ba1c91160b86f11d501ef4adf9
Umesh Pandey <upandey3@myseneca.ca>
2025-02-07 03:05:00 -0500

    Add all changes and new files

--- lab2e.py
+++ lab2e.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-# Author: Aashis Kharel
-# Author ID: akharel2
-# Date Created: 2025/02/06
+# Author: Umesh Pandey
+# Author ID: upaney3
+# Date Created: 2025/02/07
 
 timer = 10
 while timer != 0:
